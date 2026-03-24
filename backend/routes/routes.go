f0cc9d90f59a33c3340db0b81c61b1fc2f32a6de
tharunkamsala <tharunkamsala24@gmail.com>
2026-03-24 11:25:31 -0400

    model and service go files

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -13,6 +13,9 @@ func Setup(
 	router *gin.Engine,
 	authHandler *handlers.AuthHandler,
 	walletHandler *handlers.WalletHandler,
+	transferHandler *handlers.TransferHandler,
+	billHandler *handlers.BillHandler,
+	rewardHandler *handlers.RewardHandler,
 	tokenService *services.TokenService,
 ) {
 	api := router.Group("/api/v1")
@@ -37,5 +40,34 @@ func Setup(
 		wallet.POST("/add", walletHandler.AddMoney)
 		wallet.POST("/withdraw", walletHandler.Withdraw)
 		wallet.GET("/transactions", walletHandler.GetTransactions)
+	}
+
+	// Transfer routes (protected)
+	transfer := api.Group("/transfer")
+	transfer.Use(middleware.AuthMiddleware(tokenService))
+	{
+		transfer.POST("/send", transferHandler.SendMoney)
+		transfer.GET("/contacts", transferHandler.GetRecentContacts)
+		transfer.GET("/search", transferHandler.SearchUsers)
+	}
+
+	// Bills routes (protected)
+	bills := api.Group("/bills")
+	bills.Use(middleware.AuthMiddleware(tokenService))
+	{
+		bills.GET("/categories", billHandler.GetCategories)
+		bills.GET("/billers", billHandler.GetBillers)
+		bills.POST("/pay", billHandler.PayBill)
+		bills.GET("/saved", billHandler.GetSavedBillers)
+		bills.DELETE("/saved/:id", billHandler.RemoveSavedBiller)
+	}
+
+	// Rewards routes (protected)
+	rewards := api.Group("/rewards")
+	rewards.Use(middleware.AuthMiddleware(tokenService))
+	{
+		rewards.GET("", rewardHandler.GetSummary)
+		rewards.GET("/history", rewardHandler.GetHistory)
+		rewards.GET("/offers", rewardHandler.GetOffers)
 	}
 }
